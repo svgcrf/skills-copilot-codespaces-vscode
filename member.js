cc4d352dc06c565e2ae2381b0cc4f9880d14a873
Alexander Cevallos <bacmcorreo@icloud.com>
2025-11-21 20:54:11 +0000

    member

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsMember() {
+    return [
+        'JavaScript',
+        'Node.js',
+        'React',
+        'Angular',
+        'Vue.js',
+        'HTML5',
+        'CSS3',
+        'Sass   ',
+        'LESS',
+        'Responsive Web Design',
+        'Webpack',
+        'Babel',
+        'TypeScript',
+        'Git',
+        'RESTful APIs',
+        'GraphQL',
+        'Agile Methodologies',
+        'Unit Testing (Jest, Mocha)',
+        'End-to-End Testing (Cypress, Selenium)',
+        'CI/CD Pipelines',
+        'Docker',
+        'Kubernetes',
+        'AWS',
+        'Azure',
+        'Firebase'
+    ];
+}
